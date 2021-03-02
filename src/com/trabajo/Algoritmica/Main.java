d6dcaa8c8816c633c4b7c9f7912c06ecc983c1d7
Txoka <asiertxoka.2@gmail.com>
2021-03-02 12:20:56 +0100

    Initialization done

--- src/com/trabajo/Algoritmica/Main.java
+++ src/com/trabajo/Algoritmica/Main.java
@@ -1,19 +1,27 @@
 package com.trabajo.Algoritmica;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println(123);
+        Random rand=new Random();
+        int[] list10    =new int[10];
+        int[] list1000  =new int[1000];
+        int[] list100000=new int[100000];
+        for(int i=0;i<10;i++)list10[i]=rand.nextInt();
+        for(int i=0;i<1000;i++)list1000[i]=rand.nextInt();
+        for(int i=0;i<100000;i++)list100000[i]=rand.nextInt();
     }
     void insertionSort(int[] arr){
         int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
+        for (int i=1;i<n;i++){
+            int key=arr[i];
+            int j=i-1;
+            while (j>=0&&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
             }
-            arr[j + 1] = key;
+            arr[j+1]=key;
         }
     }
 }
